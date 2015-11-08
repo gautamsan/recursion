fdd4142e61a98cf9d333741c30fbbfbe9f0bc08d
Santosh Gautam <santoshgautam7@gmail.com>
2015-11-07 16:56:13 -0800

    Implemented getElementsByClassName and passed all tests.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
-};
+  var elementsArr = [];
+  function findElement(parent, foundElements, className) {
+  	var node = parent;
+  	if(node.nodeType == document.ELEMENT_NODE) { 
+	  	if(node.classList.contains(className)) {
+	  		elementsArr.push(node);
+	  	} 
+		for(var i = 0; i < node.children.length; i++) {
+	  		if(node.children[i].classList.contains(className)) {
+	  			elementsArr.push(node.children[i]);
+	  		}
+	  		//Recurse if the grandchild is present
+	  		if(node.children[i].children.length > 0) {
+		  		findElement(node.children[i], elementsArr, className)
+		  	}
+	  	}
+  	}
+  }
+  findElement(document.body, elementsArr, className);
+  console.log(elementsArr);
+  return elementsArr;
+}
